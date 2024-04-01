63c1c5d02d9c477206477049990f283f837eed42
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-01 16:26:31 -0300

    Import api project
    Co-authored-by: EgydioBNeto <egydiobolonhezi@gmail.com>

---
+++ api/prisma/migrations/20240401194747_setup/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+    "maximum_attendees" INTEGER
+);
+
+-- CreateTable
+CREATE TABLE "attendees" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "event_id" TEXT NOT NULL,
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "check_ins" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "attendee_id" INTEGER NOT NULL,
+    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
